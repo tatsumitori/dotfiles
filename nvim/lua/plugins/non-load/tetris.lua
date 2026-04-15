acbc8e5c6b13d5df52f0c09ccebf394ec5ab369c
tatsumitori <kou9782@gmail.com>
2026-04-15 09:36:37 +0900

    Add: nvim dotfile.

---
+++ nvim/lua/plugins/non-load/tetris.lua
@@ -0,0 +1,9 @@
+-- A Neovim Plugin that provides a playable text-based Tetris game.
+return {
+  "leonardo-luz/tetris.nvim",
+  lazy = true,
+  -- keys = {
+  --   { "Tetris", mode = "c" },
+  -- },
+  cmd = { "Tetris" },
+}
